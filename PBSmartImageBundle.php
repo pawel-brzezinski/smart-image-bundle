85e59a6983e1462200e9ee1da32b4d872d2288cb
Paweł Brzeziński <pawel.brzezinski@smartint.pl>
2020-01-27 18:16:58 +0100

    Initial commit

---
+++ PBSmartImageBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PB\Bundle\SmartImageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Paweł Brzeziński <pawel.brzezinski@db-team.pl>
+ */
+final class PBSmartImageBundle extends Bundle
+{
+
+}
